c3db8d7c2b16947c6e9b8080fa689185f92405e4
lyc <liuyancao@126.com>
2022-02-26 18:21:52 +0800

    add demo

---
+++ src/main/kotlin/cn/sople/001.kt
@@ -0,0 +1,21 @@
+package cn.sople.learn01
+//声明变量与内置数据类型
+fun main(){
+    println("Hello World")
+    //变量声明
+    var name: String = "Hello Sople"
+    println("name is : $name")
+    //内置数据类型
+    /**
+     * String
+     * Char
+     * Boolean
+     * Int
+     * Long
+     * Float
+     * Double
+     * List
+     * Set
+     * Map
+     */
+}
---
+++ src/main/kotlin/cn/sople/002.kt
@@ -0,0 +1,23 @@
+package cn.sople
+
+/**
+ * 只读变量
+ * 类型推断
+ * 编译时常量
+ */
+//编译时常量，只能定义于函数之外，函数之内的变量是在运行时被赋值的。
+//编译时常量是在编译时被赋值
+const val PI = 3.1415926
+fun main(){
+    //val 变量一旦被赋值，就无法再被改变。程序开发过程中尽量使用val去代替var,更安全。
+    val info:String = "my info"
+    println("info is : $info")
+    //kotlin类型推断：编译器根据变量实际值推断变量类型，变量类型一旦确定就不可以改变。
+    val name = "this is a string"
+    println(name)
+    val age = 35
+    println(age)
+    val letter = 'A'
+    println(letter)
+
+}
